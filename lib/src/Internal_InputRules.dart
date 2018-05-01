7a67063cd50f44054b4b8252a3835e3810e55947
ferrerj <ferrerj752@gmail.com>
2018-05-01 00:00:39 -0400

    Beginning to flesh out structures for input rules
    More info on input rules, they will act as a wrapper for the function and make sure the data is valid and (SQL) safe to provide to the function to return data, with the end goal of reducing that work for the programmer. I hope to eventually bring this to make easier SQL queries.

--- lib/src/Internal_InputRules.dart
+++ lib/src/Internal_InputRules.dart
@@ -15,10 +15,43 @@ abstract class InputDataRule extends InputRule {
   final dynamic arg; // either arg position number or arg name
   final String name; // name of the input variable for function to be called
   const InputDataRule(this.dataInputType, this.arg, this.name);
+  // gets the data point based on how it is stored and returns it as a string to be parsed by the InputType
+  extractDataPoint(List cookies, dynamic getData, String postData);
 }
-// Declares how the arguments are layed out and are as expected, breaks them up as necessary
+// Declares how the arguments are laid out and are as expected, breaks them up as necessary
 abstract class InputTypeRule extends InputRule {
   const InputTypeRule();
   processInputs(String get, String post, List cookie); // generate functions to check inputs
   countInputs(); // make sure there are enough inputs and declared inputs
+}
+
+abstract class GetData extends InputDataRule{
+  GetData(String arg, String name) : super(DataInputType.GET, arg, name);
+}
+
+abstract class PostData extends InputDataRule {
+  PostData(String arg, String name) : super(DataInputType.POST, arg, name);
+}
+// this is only a key store, shouldn't really have JSON or other formating to seperate out the different content fields
+abstract class CookieData extends InputDataRule {
+  CookieData(String arg, String name) : super(DataInputType.COOKIE, arg, name);
+}
+
+
+// these are used to reduce the amount of work in isolating data from the above classes
+// these will be used as mixins with the above classes
+abstract class FromJSON {
+  // from data stored in a JSON object
+}
+
+abstract class FromCSV {
+  // from comma seperated data
+}
+
+abstract class FromSlashData {
+  // stored as /name/data
+}
+// really going to start with this one (for personal projects) and will do the others next
+abstract class FromAndData {
+  // stored as ?var1=data1&
 }
